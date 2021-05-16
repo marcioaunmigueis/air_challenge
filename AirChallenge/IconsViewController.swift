26bd039537dba658972dfea33647f0a21bec8f4f
Marcio Aun Migueis <marcio@ar1mobile.com.br>
2021-05-16 11:58:17 -0300

    Create IconsViewController and related test file

--- AirChallenge/IconsViewController.swift
+++ AirChallenge/IconsViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IconsViewController: UIViewController {
 
+  @IBOutlet var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
 
 
---
+++ AirChallengeTests/IconsViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  IconsViewControllerTests.swift
+//  AirChallengeTests
+//
+//  Created by Marcio Migueis on 16/05/21.
+//
+
+import XCTest
+
+class IconsViewControllerTests: XCTestCase {
+
+  func test_failure() {
+    XCTFail("just for verify test ")
+  }
+
+}
