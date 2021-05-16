de8f75e1cbb504f67ffc05979be6cc383c262cc2
Marcio Aun Migueis <marcio@ar1mobile.com.br>
2021-05-16 14:09:55 -0300

    Create Icon model

---
+++ AirChallenge/Models/Icon.swift
@@ -0,0 +1,15 @@
+//
+//  Icon.swift
+//  AirChallenge
+//
+//  Created by Marcio Migueis on 16/05/21.
+//
+import UIKit
+import Foundation
+
+struct Icon: Decodable {
+  var title: String
+  var subtitle: String
+  var image: String
+  
+}
